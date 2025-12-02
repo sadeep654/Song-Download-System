cc00156f4bd49426840bb4e2f8d129226f701ab0
Sadeep Dilshan Kasthuriarachchi <76505825+sadeep654@users.noreply.github.com>
2025-12-02 14:40:53 +0530

    Update Artist.java

--- src/Artist.java
+++ src/Artist.java
@@ -1,2 +1,33 @@
-// Artist.java template
-public class Artist {}
+public class Artist {
+    private int artistId;
+    private String name;
+
+    public Artist() {}
+
+    public Artist(int artistId, String name) {
+        this.artistId = artistId;
+        this.name = name;
+    }
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return artistId + " | " + name;
+    }
+}
