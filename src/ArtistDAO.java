422e23212fa6af21f213fd5141af0d1d92734497
Sadeep Dilshan Kasthuriarachchi <76505825+sadeep654@users.noreply.github.com>
2025-12-02 14:37:08 +0530

    Update ArtistDAO.java

--- src/ArtistDAO.java
+++ src/ArtistDAO.java
@@ -1,2 +1,16 @@
-// ArtistDAO.java template
-public class ArtistDAO {}
+public class ArtistDAO {
+
+    public int addArtist(String name) throws Exception {
+        Connection con = DBConnection.getConnection();
+        String query = "INSERT INTO artists (name) VALUES (?)";
+        PreparedStatement pst = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        pst.setString(1, name);
+        pst.executeUpdate();
+
+        ResultSet rs = pst.getGeneratedKeys();
+        rs.next();
+        int id = rs.getInt(1);
+        con.close();
+        return id;
+    }
+}
