246958872800a94fccc8d9c162a682e9532c5193
Sadeep Dilshan Kasthuriarachchi <76505825+sadeep654@users.noreply.github.com>
2025-12-02 14:36:15 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,2 +1,12 @@
-// DBConnection.java template
-public class DBConnection {}
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/musicdb";
+    private static final String USER = "root";
+    private static final String PASS = "your_password";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
